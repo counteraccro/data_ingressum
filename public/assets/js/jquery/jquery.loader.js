2c42fee4671c6ce3ca7b2117169007958e768f0f
counteraccro <counteraccro@gmail.com>
2020-05-16 23:29:13 +0200

    avancement plugin jquery loader

--- public/assets/js/jquery/jquery.loader.js
+++ public/assets/js/jquery/jquery.loader.js
@@ -6,11 +6,19 @@
 (function($) {
   $.fn.showLoader = function()
   {
-	  $(this).append('<div class="text-center float-right">' +
-			'<div><i>Chargement...</i></div>' +
-		  '<div class="spinner-border spinner-border-sm text-secondary" role="status">' +
+	  var height = $(this).height();
+	  var width = $(this).width();
+	  
+	  var rand = Math.floor((Math.random() * 10) + 1);
+	  
+	  var str_loading = ['Chargement...', 'Loading...', 'Please wait...', '42...', 'Autodestruction...', 'ça arrive...', 'Merci de patienter...', '2 secondes please...', 'Nop !', 'Ho.. wait !', 'Paf le chien']
+	  
+	  $(this).prepend('<div style="position:absolute; width:' + width + 'px; height:' + height + 'px; background-color:#FAFAFA; opacity:0.7; z-index:1000; filter:alpha(opacity=70);">' +
+			  '<div class="text-center align-items-center" style="z-index:1001;color:black;margin-top:'+ ((height/2)-10) +'px;">' +
+			'<i>' + str_loading[rand] + '</i> ' +
+		  '<div class="spinner-border spinner-border-sm text-info" role="status">' +
 		    '<span class="sr-only">Loading...</span>' +
 		  '</div>' +
-		  '</div>');
+		  '</div></div>');
   };
 })(jQuery);
