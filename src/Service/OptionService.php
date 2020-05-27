b6cd30ac7a3b6f51b637474ddd2bd9e858aea7dc
counteraccro <counteraccro@gmail.com>
2020-05-27 22:55:01 +0200

    Avancement option

--- src/Service/OptionService.php
+++ src/Service/OptionService.php
@@ -4,6 +4,7 @@ namespace App\Service;
 use App\Entity\User;
 use App\Entity\Option;
 use App\Entity\OptionUser;
+use Doctrine\Common\Persistence\ManagerRegistry as Doctrine;
 
 /**
  * Class qui va gérer les options du joueur
@@ -16,18 +17,21 @@ class OptionService
 
     /**
      * Option auto_save
+     *
      * @var string
      */
     private $option_auto_save = 'auto_save';
 
     /**
      * Type oui / non
+     *
      * @var integer
      */
     private $type_yes_no = 1;
 
     /**
      * Options par defaut
+     *
      * @var array
      */
     private $defaultOptions = [
@@ -39,40 +43,55 @@ class OptionService
             'setDefaultValue' => 1
         ]
     ];
-    
+
     private $tmp_defaultOptions = [];
 
     /**
+     *
+     * @var Doctrine
+     */
+    private $doctrine;
+
+    public function __construct(Doctrine $doctrine)
+    {
+        $this->doctrine = $doctrine;
+    }
+
+    /**
      * Permet de générer les options par default et de les ajouter au user
-     * 
+     * Si l'option existe déjà elle n'est pas recréé
+     *
      * @param User $user
      * @return \App\Entity\User
      */
     public function createDefaultOption(User $user)
     {
-        if(empty($this->tmp_defaultOptions))
-        {
-            foreach($this->defaultOptions as $op)
-            {
-                $option = new Option();
-                foreach($op as $key => $value)
-                {
-                    $option->{$key}($value);
+        if (empty($this->tmp_defaultOptions)) {
+            foreach ($this->defaultOptions as $op) {
+                $option = $this->doctrine->getRepository(Option::class)->findBy(array(
+                    'name' => $op['setName']
+                ));
+
+                if (! is_object($option)) {
+                    $option = new Option();
+                    foreach ($op as $key => $value) {
+                        $option->{$key}($value);
+                    }
+                    
+                    array_push($this->tmp_defaultOptions, $option);
                 }
-                array_push($this->tmp_defaultOptions, $option);
             }
         }
-        
-        foreach($this->tmp_defaultOptions as $option)
-        {
+
+        foreach ($this->tmp_defaultOptions as $option) {
             $optionUser = new OptionUser();
             $optionUser->setUser($user);
             $optionUser->setOptionData($option);
             $optionUser->setValue($option->getDefaultValue());
-            
+
             $user->addOptionUser($optionUser);
         }
-        
+
         return $user;
     }
 }
